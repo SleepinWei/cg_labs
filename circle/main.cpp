eda1c09298d38ebbf0e74d292e14727dfc54aaab
SleepinWei <zyw0241@outlook.com>
2022-09-29 01:14:48 +0800

    A: implement a demo to draw circles
    This is an interactive demo to draw circles
    the demo features bresenham algorithm

---
+++ circle/main.cpp
@@ -0,0 +1,226 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+#include<vector>
+#include<memory>
+#include"./shader/Shader.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button(GLFWwindow* window, int, int, int);
+void processInput(GLFWwindow* window);
+unsigned int loadTexture(const char *path);
+int createWindow(GLFWwindow*& window, int width, int height, std::string title = "Demo");
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+int gladInit();
+
+int g_xpos, g_ypos; 
+// std::vector<int> vertices = {};
+// std::vector<unsigned int> VAOs = {};
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+struct Point {
+    int x, y; // pixel position on screen
+
+    Point operator+(Point p){
+        return {p.x + x, p.y + y};
+    }
+
+    Point operator-(Point p){
+        return {x-p.x,y-p.y};
+    }
+};
+
+class Screen{
+public:
+    std::vector<float> vertices;
+    GLuint VAO;
+    GLuint VBO;
+    bool dirty; 
+
+    Screen(){
+        dirty = true; 
+    }
+
+    void draw(){
+        if(dirty){
+            if(VAO){
+                glDeleteVertexArrays(1,&VAO);
+                glDeleteBuffers(1,&VBO);
+            }
+            glGenVertexArrays(1,&VAO);
+            glGenBuffers(1,&VBO);
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER,VBO);
+            glBufferData(GL_ARRAY_BUFFER,vertices.size()*sizeof(float),&vertices[0],GL_STATIC_DRAW);
+            glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float),(void*)0);
+            glEnableVertexAttribArray(0);
+            dirty = false;
+            for(int i= 0;i<vertices.size();i++)
+                std::cout << vertices[i] <<'\n';
+        }
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS,0,vertices.size()/2);
+    }
+
+    void putPixel(const Point& p){
+        vertices.push_back(p.x * 2.0 / SCR_WIDTH - 1);
+        vertices.push_back(-p.y * 2.0 / SCR_HEIGHT + 1);
+        dirty = true;
+    }
+};
+
+void createCircle(Screen & screen, Point c, float r, float pointSize = 5){
+    /*
+    * c: center, r: radius
+    * draw a pixel at position p: screen.putPixel(Point p) 
+    * notice: p refers to the position relative to the bottom-left in pixels
+    */
+    //TODO 
+    std::cout << "CreateCircle"  << " Center: " << c.x << ' ' << c.y << " Radius: " << r << '\n';
+    Point p = {0,int(r)};
+    float d = 1.0f - r; 
+    screen.putPixel(c + p);
+    screen.putPixel(c+Point{p.x,-p.y});
+    screen.putPixel(c+Point{p.y,p.x});
+    screen.putPixel(c+Point{-p.y,p.x});
+
+    while(p.x <= p.y){
+        p.x += 1; 
+        if(d < 0){
+            p.y = p.y; 
+            d += 2 * p.x + 1; 
+        }
+        else{
+            p.y = p.y -1; 
+            d += 2 * p.x + 1 - 2 * p.y; 
+        }
+        screen.putPixel(c + Point{p.x,p.y});
+        screen.putPixel(c + Point{p.x,-p.y});
+        screen.putPixel(c + Point{-p.x,-p.y});
+        screen.putPixel(c + Point{-p.x,p.y});
+        screen.putPixel(c + Point{p.y,p.x});
+        screen.putPixel(c + Point{p.y,-p.x});
+        screen.putPixel(c + Point{-p.y,-p.x});
+        screen.putPixel(c + Point{-p.y,p.x});
+    }
+}
+
+// screen
+Screen screen; 
+
+void mainloop() {
+	glfwInit();
+	GLFWwindow* window; 
+	createWindow(window, SCR_WIDTH, SCR_HEIGHT);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	//glad
+	gladInit();
+
+    Shader shader("./shader/shader.vs", "./shader/shader.fs");
+    shader.use();
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+	while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(.6,.2,.0,1.0);
+        processInput(window);
+        shader.use();
+        screen.draw();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+    }
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+int main() {
+    mainloop();
+}
+
+int gladInit() {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize" << std::endl;
+        return -1;
+    }
+    return 0;
+}
+
+int createWindow(GLFWwindow*& window,
+    int width,
+    int height,
+    std::string title) {
+    //create a glfw window 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //create window
+    window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetMouseButtonCallback(window, mouse_button);
+    //glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    static bool enableCursor = false;
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void addCircle(Screen& screen, Point center, Point p) {
+    float radius = sqrt((center.x - p.x)*(center.x-p.x) + (center.y - p.y)*(center.y -p.y));
+    createCircle(screen,center,radius);
+}
+
+void mouse_button(GLFWwindow* window, int button, int action,int mods){
+    static int click_cnt = 0; 
+    static Point c; 
+    static Point p; 
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        click_cnt += 1; 
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        // std::cout << "Click Count: " << click_cnt << '\n';
+        if(click_cnt == 1){
+            c = {int(xpos),int(ypos)};
+        }
+        if(click_cnt == 2){
+            p = {int(xpos),int(ypos)};
+            addCircle(screen, c, p);
+            click_cnt = 0;
+        }
+    }
+}
---
+++ circle/shader/Shader.cpp
@@ -0,0 +1,223 @@
+#include<glad/glad.h>
+#include"./Shader.h"
+#include<GLFW/glfw3.h>
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath,const char* tessControlPath,const char* tessEvalPath)
+{
+    // 1. retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::string geometryCode;
+    std::string tessControlCode;
+    std::string tessEvalCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    std::ifstream gShaderFile;
+    std::ifstream tessCShaderFile;
+    std::ifstream tessEShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    tessCShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    tessEShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        // open files
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        // read file's buffer contents into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+        // if geometry shader path is present, also load a geometry shader
+        if (geometryPath != nullptr)
+        {
+            gShaderFile.open(geometryPath);
+            std::stringstream gShaderStream;
+            gShaderStream << gShaderFile.rdbuf();
+            gShaderFile.close();
+            geometryCode = gShaderStream.str();
+        }
+        if (tessControlPath != nullptr) {
+            tessCShaderFile.open(tessControlPath);
+            std::stringstream tcShaderStream;
+            tcShaderStream << tessCShaderFile.rdbuf();
+            tessCShaderFile.close();
+            tessControlCode = tcShaderStream.str();
+        }
+        if (tessEvalPath != nullptr) {
+            tessEShaderFile.open(tessEvalPath);
+            std::stringstream teShaderStream;
+            teShaderStream << tessEShaderFile.rdbuf();
+            tessEShaderFile.close();
+            tessEvalCode = teShaderStream.str();
+        }
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    // vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // fragment Shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+    // if geometry shader is given, compile geometry shader
+    unsigned int geometry;
+    if (geometryPath != nullptr)
+    {
+        const char* gShaderCode = geometryCode.c_str();
+        geometry = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry, 1, &gShaderCode, NULL);
+        glCompileShader(geometry);
+        checkCompileErrors(geometry, "GEOMETRY");
+    }
+    unsigned int tessControl, tessEval;
+    if (tessControlPath != nullptr) {
+        const char* tcShaderCode = tessControlCode.c_str();
+        tessControl = glCreateShader(GL_TESS_CONTROL_SHADER);
+        glShaderSource(tessControl, 1,&tcShaderCode, NULL);
+        glCompileShader(tessControl);
+        checkCompileErrors(tessControl, "TESS CONTROL");
+    }
+    if (tessEvalPath != nullptr) {
+        const char* teShaderCode = tessEvalCode.c_str();
+        tessEval = glCreateShader(GL_TESS_EVALUATION_SHADER);
+        glShaderSource(tessEval, 1,&teShaderCode, NULL);
+        glCompileShader(tessEval);
+        checkCompileErrors(tessEval, "TESS EVAL");
+    }
+    // shader Program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    if (geometryPath != nullptr)
+        glAttachShader(ID, geometry);
+    if (tessControlPath != nullptr)
+        glAttachShader(ID, tessControl);
+    if (tessEvalPath != nullptr)
+        glAttachShader(ID, tessEval);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    // delete the shaders as they're linked into our program now and no longer necessery
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    if (geometryPath != nullptr)
+        glDeleteShader(geometry);
+    if (tessControlPath != nullptr)
+        glDeleteShader(tessControl);
+    if (tessEvalPath != nullptr)
+        glDeleteShader(tessEval);
+}
+// activate the shader
+// ------------------------------------------------------------------------
+void Shader::use()
+{
+    glUseProgram(ID);
+}
+// utility uniform functions
+// ------------------------------------------------------------------------
+void Shader::setBool(const std::string& name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+// ------------------------------------------------------------------------
+void Shader::setInt(const std::string& name, int value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+// ------------------------------------------------------------------------
+void Shader::setFloat(const std::string& name, float value) const
+{
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+// ------------------------------------------------------------------------
+void Shader::setVec2(const std::string& name, const glm::vec2& value) const
+{
+    glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+void Shader::setVec2(const std::string& name, float x, float y) const
+{
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
+// ------------------------------------------------------------------------
+void Shader::setVec3(const std::string& name, const glm::vec3& value) const
+{
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+void Shader::setVec3(const std::string& name, float x, float y, float z) const
+{
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+// ------------------------------------------------------------------------
+void Shader::setVec4(const std::string& name, const glm::vec4& value) const
+{
+    glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+void Shader::setVec4(const std::string& name, float x, float y, float z, float w)
+{
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+}
+// ------------------------------------------------------------------------
+void Shader::setMat2(const std::string& name, const glm::mat2& mat) const
+{
+    glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void Shader::setMat3(const std::string& name, const glm::mat3& mat) const
+{
+    glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void Shader::setMat4(const std::string& name, const glm::mat4& mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+// utility function for checking shader compilation/linking errors.
+// ------------------------------------------------------------------------
+void Shader::checkCompileErrors(unsigned int shader, std::string type)
+{
+    GLint success;
+    GLchar infoLog[1024];
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+    else
+    {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+}
+
+void Shader::setUniformBuffer(const std::string& name, int binding)const {
+    glUniformBlockBinding(ID,
+        glGetUniformBlockIndex(ID, name.c_str()),binding
+    );
+}
