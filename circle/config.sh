4bb01148a52be0bb7c4fa48319e45ff961322a81
betray12138 <2318206376@qq.com>
2022-09-29 17:22:30 +0800

    update cmake and script

--- circle/config.sh
+++ circle/config.sh
@@ -5,15 +5,23 @@ apt-get install -y libxss-dev libxxf86vm-dev libxkbfile-dev libxv-dev libx11-dev
 apt-get install -y libxrandr-dev libxi-dev libxinerama-dev libsdl2-dev libxcursor-dev
 
 # git clone glfw and compile 
-git clone https://github.com/glfw/glfw.git
-cd glfw && mkdir build && cd build && cmake .. && make -j8 && make install
-rm -rf glfw
+if [ ! -d "glfw" ]; then
+    git clone https://github.com/glfw/glfw.git
+fi
+if [ -d "glfw/build" ]; then
+    rm -rf glfw/build
+fi
+cd glfw && mkdir build && cd build && cmake .. && make -j8 && make install && cd ../../
+#rm -rf glfw
 
 # git clone glm and compile
-git clone https://github.com/g-truc/glm.git
-cd glm && mkdir build && cd build && cmake .. && make -j8 && make install
-rm -rf glm
+if [ ! -d "glm" ]; then
+    git clone https://github.com/g-truc/glm.git
+fi
+if [ -d "glm/build" ]; then
+    rm -rf glm/build
+fi
+cd glm && mkdir build && cd build && cmake .. && make -j8 && make install && cd  ../../
+#rm -rf glm
 
-chmod +x ./compile_project.sh && ./compile_project.sh
-
-
+./compile_project.sh
