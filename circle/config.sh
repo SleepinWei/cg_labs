cf99849225ce258ad054f8cc6e821500c29428f6
betray12138 <2318206376@qq.com>
2022-09-29 15:25:21 +0800

    update config.sh

--- circle/config.sh
+++ circle/config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# glfw ready work
+apt-get install -y libxss-dev libxxf86vm-dev libxkbfile-dev libxv-dev libx11-dev
+apt-get install -y libxrandr-dev libxi-dev libxinerama-dev libsdl2-dev libxcursor-dev
+
 # git clone glfw and compile 
 git clone https://github.com/glfw/glfw.git
 cd glfw && mkdir build && cd build && cmake .. && make -j8 && make install
@@ -10,5 +14,6 @@ git clone https://github.com/g-truc/glm.git
 cd glm && mkdir build && cd build && cmake .. && make -j8 && make install
 rm -rf glm
 
-chmod +x ./compile.sh && ./compile_project.sh
+chmod +x ./compile_project.sh && ./compile_project.sh
+
 
